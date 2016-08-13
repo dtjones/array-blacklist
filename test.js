01b62c9441686f0356233ed44f051393f4db7dc0
Tarran Jones <tarrandavidjones@gmail.com>
2016-08-13 20:43:43 +0100

    init

---
+++ test.js
@@ -0,0 +1,25 @@
+// var array_blacklist = require('array_blacklist');
+
+// var userInputArray = ['ok','good', 'great', 1, 'bad', 'evil', {},[],0,'zero'];
+// var defaultRestrictions = ['bad', 'evil'];
+// var moreRestrictions = [{},[],0];
+// var evenMoreRestrictions = ['zero'];
+
+// var cleanArray = array_blacklist(userInputArray, defaultRestrictions, moreRestrictions, evenMoreRestrictions);
+// console.log(cleanArray); 
+
+import test from 'ava';
+import array_blacklist from './';
+
+
+var userInputArray = ['ok','good', 'great', 1, 'bad', 'evil',0,'zero',[],{}];
+var defaultRestrictions = ['bad', 'evil'];
+var moreRestrictions = [0,[]];
+var evenMoreRestrictions = ['zero',{}];
+
+test('multiple args test', t => {
+	var cleanArray = array_blacklist(userInputArray, defaultRestrictions, moreRestrictions, evenMoreRestrictions);
+    t.deepEqual(cleanArray, [ 'ok', 'good', 'great', 1]);
+});
+
+
